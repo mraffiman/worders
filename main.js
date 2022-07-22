7cc3abd3a98045a5fb6802ad97d8132c8cab6859
mraffiman <107804396+mraffiman@users.noreply.github.com>
2022-07-22 21:25:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function one(){
+    window.alert("The Taj Mahal is an Islamic ivory-white marble mausoleum on the right bank of the river Yamuna in the Indian city of Agra.")
+}
+
+function two(){
+    window.alert("The Great Wall of China is a series of fortifications that were built across the historical northern borders of ancient Chinese states and Imperial China as protection against various nomadic groups from the Eurasian Steppe.");
+}
+
+function three(){
+    window.alert("Machu Picchu is an Incan citadel set high in the Andes Mountains in Peru, above the Urubamba River valley.");
+}
+
+function four(){
+    window.alert("The Colosseum is an oval amphitheatre in the centre of the city of Rome, Italy, just east of the Roman Forum. It is the largest ancient amphitheatre ever");
+}
+
+function five(){
+    window.alert("Petra is a famous archaeological site in Jordan's southwestern desert. Dating to around 300 B.C., it was the capital of the Nabatean Kingdom.");
+}
+
+function six(){
+    window.alert("Chichen Itza was the most populous city in the entire Yucatan Peninsula. Even today, its ruins reflect a great civilization filled with architectural marvels and ancient secrets. ");
+}
+
+function seven(){
+    window.alert("The statue of Christ the Redeemer has become a symbolic protector of people. Like Jesus Christ, the statue protects the urban environment, like a roof over your head.");
+}
+
+
+
